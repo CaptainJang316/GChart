3086bb8e3f7db5d10221c983eb07729b362ba710
Gyeongsu <gksrkfkaryghl@gmail.com>
2024-11-03 22:56:39 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
+/* eslint-disable */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LineChart from './components/Chart/LineChart';
 
 function App() {
+  const sampleData = [
+    { label: 'A', value: 10 },
+    { label: 'B', value: 20 },
+    { label: 'C', value: 30 },
+    { label: 'D', value: 40 },
+    { label: 'E', value: 50 },
+    { label: 'F', value: 15 },
+    { label: 'G', value: 80 },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sample Chart</h1>
+      {/* 차트의 border 없애고, X,Y 축에 대한 컴포넌트들로 더 큰 바운더리 구현해야 함. */}
+      {/* => ㄴㄴ. 그냥 단일 SVG로 구현하는 게 훨씬 좋음. */}
+      <div className="chart-wrapper">
+        <LineChart data={sampleData} width={500} height={400} color="#FF6347"/>
+      </div>
     </div>
   );
 }
