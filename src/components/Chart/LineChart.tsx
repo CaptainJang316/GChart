1567bc3e3dcd26b422e568a1b803eadad4f34974
Gyeongsu <gksrkfkaryghl@gmail.com>
2024-12-12 23:54:08 +0900

    Update

--- src/components/Chart/LineChart.tsx
+++ src/components/Chart/LineChart.tsx
@@ -152,7 +152,7 @@ const LineChart: React.FC<LineChartProps> = ({
   title,
   layout: { width = 500, height = 300, padding = 15 },
   axis,
-  chartStyle: { fillArea = false, lineStyle = 'solid' },
+  chartStyle: { fillArea = false, lineStyle = "solid" },
   tooltip = true,
 }) => {
   const lineData = data.data.map((item) => item.value);
@@ -327,40 +327,40 @@ const LineChart: React.FC<LineChartProps> = ({
     let pathData = "";
     const currXValues: number[] = [];
     let currX = axis?.xAxis?.boundaryGap ? barWidth / 2 + 25 : 25;
-  
+
     // X 좌표 배열 생성
     currLineValues.forEach(() => {
       currXValues.push(currX);
       currX += barWidth;
     });
-  
+
     // M 명령으로 시작점 설정
     pathData = `M ${currXValues[0]} ${scales.yScale(currLineValues[0])}`;
-  
+
     for (let i = 0; i < currLineValues.length - 1; i++) {
       const x0 = currXValues[i - 1] || currXValues[i]; // 이전 X
       const y0 = scales.yScale(currLineValues[i - 1] || currLineValues[i]); // 이전 Y
-  
+
       const x1 = currXValues[i]; // 현재 X
       const y1 = scales.yScale(currLineValues[i]); // 현재 Y
-  
+
       const x2 = currXValues[i + 1]; // 다음 X
       const y2 = scales.yScale(currLineValues[i + 1]); // 다음 Y
-  
+
       const x3 = currXValues[i + 2] || x2; // 다다음 X
       const y3 = scales.yScale(currLineValues[i + 2] || currLineValues[i + 1]); // 다다음 Y
-  
+
       // 제어점 계산 (Catmull-Rom 스플라인)
       const cp1X = x1 + (x2 - x0) / 6;
       const cp1Y = y1 + (y2 - y0) / 6;
-  
+
       const cp2X = x2 - (x3 - x1) / 6;
       const cp2Y = y2 - (y3 - y1) / 6;
-  
+
       // 곡선 연결
       pathData += ` C ${cp1X} ${cp1Y}, ${cp2X} ${cp2Y}, ${x2} ${y2}`;
     }
-  
+
     return pathData;
   };
 
@@ -394,36 +394,36 @@ const LineChart: React.FC<LineChartProps> = ({
     const currXValues: number[] = [];
     let currX = axis?.xAxis?.boundaryGap ? barWidth / 2 + 25 : 25;
     let firstX = currX;
-  
+
     // X 좌표 배열 생성
     currLineValues.forEach(() => {
       currXValues.push(currX);
       currX += barWidth;
     });
-    let x2 = 0;
+
     // M 명령으로 시작점 설정
     pathData = `M ${currXValues[0]} ${scales.yScale(currLineValues[0])}`;
-  
+
     for (let i = 0; i < currLineValues.length - 1; i++) {
       const x0 = currXValues[i - 1] || currXValues[i]; // 이전 X
       const y0 = scales.yScale(currLineValues[i - 1] || currLineValues[i]); // 이전 Y
-  
+
       const x1 = currXValues[i]; // 현재 X
       const y1 = scales.yScale(currLineValues[i]); // 현재 Y
-  
-      x2 = currXValues[i + 1]; // 다음 X
+
+      const x2 = currXValues[i + 1]; // 다음 X
       const y2 = scales.yScale(currLineValues[i + 1]); // 다음 Y
-  
+
       const x3 = currXValues[i + 2] || x2; // 다다음 X
       const y3 = scales.yScale(currLineValues[i + 2] || currLineValues[i + 1]); // 다다음 Y
-  
+
       // 제어점 계산 (Catmull-Rom 스플라인)
       const cp1X = x1 + (x2 - x0) / 6;
       const cp1Y = y1 + (y2 - y0) / 6;
-  
+
       const cp2X = x2 - (x3 - x1) / 6;
       const cp2Y = y2 - (y3 - y1) / 6;
-  
+
       console.log("currX: " + currX + ", x2: " + x2);
       // 곡선 연결
       pathData += ` C ${cp1X} ${cp1Y}, ${cp2X} ${cp2Y}, ${x2} ${y2}`;
@@ -431,7 +431,7 @@ const LineChart: React.FC<LineChartProps> = ({
 
     // Area 만들기 위한 작업(path 닫기)
     // 1. 마지막 지점에서 아래로
-    pathData += ` L ${x2} ${height}`;
+    pathData += ` L ${currXValues[currXValues.length - 1]} ${height}`;
     // 2. 시작점으로 돌아가기
     pathData += ` L ${firstX} ${height} Z`;
 
@@ -630,7 +630,11 @@ const LineChart: React.FC<LineChartProps> = ({
               (v, i) =>
                 fillArea && (
                   <StyledPathArea
-                    d={lineStyle == 'solid'? createSolidAreaPath(v) : createSmoothAreaPath(v)}
+                    d={
+                      lineStyle == "solid"
+                        ? createSolidAreaPath(v)
+                        : createSmoothAreaPath(v)
+                    }
                     isVisible={isVisible}
                     color={`${hoveredLineIndex == i ? darkenColor(lineColors[i], 0.2) : lineColors[i]}`}
                     onMouseOver={() => setHoveredLineIndex(i)}
@@ -644,7 +648,11 @@ const LineChart: React.FC<LineChartProps> = ({
             lineData.map((v, i) => (
               <StyledPath
                 ref={(el) => addPathRef(i, el)}
-                d={lineStyle == 'solid'? createSolidLinePath(v) : createSmoothLinePath(v)}
+                d={
+                  lineStyle == "solid"
+                    ? createSolidLinePath(v)
+                    : createSmoothLinePath(v)
+                }
                 isVisible={isVisible}
                 totalLength={pathLength[i]}
                 color={
